c971b1a83e26305e364c4ea655528bb8e1946049
mingfei <mingfei.liao@pactera.com>
2021-01-28 18:14:31 +0800

    修改echarts组件封装

--- src/components/echart/echatr.vue
+++ src/components/echart/echatr.vue
@@ -17,28 +17,49 @@ export default {
       type: Boolean,
       default: true,
     },
-    colorList: {
-      type: Array,
-      default: () => {
-        return [
-          "#C1232B",
-          "#B5C334",
-          "#FCCE10",
-          "#E87C25",
-          "#27727B",
-          "#FE8463",
-          "#9BCA63",
-          "#FAD860",
-        ];
-      },
-    },
   },
   data() {
     return {
       echart: null,
       echart3D: {
+        // label: {
+        //   show: true, //开启显示
+        //   position: "top", //在上方显示
+        //   textStyle: {
+        //     //数值样式
+        //     color: "black",
+        //     fontSize: ,
+        //     fontWeight: 600,
+        //   },
+        // },
+        title: {
+          text: "",
+          textStyle: {
+            // color: "red",
+          },
+          padding: [10, 0, 10, 100], // 位置
+        },
+        legend: {
+          selectedMode: false, //禁用点击事件
+          type: "plain", // 图列类型，默认为 'plain'
+          right: "10%",
+          top: 10,
+          textStyle: {
+            // 图列内容样式
+            color: "#ccc", // 字体颜色
+          },
+          tooltip: {
+            // 图列提示框，默认不显示
+            show: true,
+            color: "red",
+          },
+        },
+        // 鼠标放上效果
         tooltip: {
           trigger: "axis",
+          textStyle: {
+            align: "left",
+          },
           axisPointer: {
             // 坐标轴指示器，坐标轴触发有效
             type: "shadow", // 默认为直线，可选为：'line' | 'shadow'
@@ -46,17 +67,19 @@ export default {
         },
         // echart  距离周边的距离
         grid: {
-          left: "2%",
-          right: "2%",
-          top: "10%",
-          bottom: "18%",
+          left: "3%",
+          right: "3%",
+          top: "15%",
+          bottom: "5%",
+          containLabel: true,
         },
+        // x轴效果
         xAxis: [
           {
             axisLabel: {
               show: true,
               textStyle: {
-                color: "#aaaaaa",
+                color: "#000000",
                 fontSize: 12,
               },
               margin: 20, //刻度标签与轴线之间的距离。
@@ -73,125 +96,37 @@ export default {
               width: 0.08,
               lineStyle: {
                 type: "solid",
-                color: "#03202E",
+                color: "#000000",
               },
             },
           },
         ],
+        // y 轴效果
         yAxis: [
           {
-            show: true, //
+            show: true, //不显示y轴
             splitLine: {
-              show: true,
+              show: true, //false 隐藏 网格
               lineStyle: {
+                //网格样式
                 color: "#eee",
                 type: "solid",
               },
             },
-            axisTick: {
-              show: false,
-            },
-            axisLine: {
-              show: false,
-            },
+            // axisTick: {
+            //   show: false,
+            // },
+            // axisLine: {
+            //   show: false,
+            // },
             axisLabel: {
               textStyle: {
-                color: "#fff",
+                color: "",
               },
             },
           },
         ],
-        series: [
-          {
-            label: {
-              show: true, //开启显示
-              position: "top", //在上方显示
-              textStyle: {
-                //数值样式
-                color: "black",
-                fontSize: 16,
-                fontWeight: 600,
-                padding: 20,
-                // top:80,
-              },
-            },
-            //柱底圆片
-            name: "",
-            type: "pictorialBar",
-            symbolSize: [60, 22], //调整截面形状
-            symbolOffset: [0, 10],
-            z: 12,
-            itemStyle: {
-              normal: {
-                color: (params) => {
-                  var colorArr = [
-                    "#C1232B",
-                    "#B5C334",
-                    "#FCCE10",
-                    "#E87C25",
-                    "#27727B",
-                    "#FE8463",
-                    "#9BCA63",
-                  ];
-                  return colorArr[params.dataIndex];
-                },
-              },
-            },
-          },
-
-          //柱体
-          {
-            name: "",
-            type: "bar",
-            barWidth: 60,
-            barGap: "0%",
-            itemStyle: {
-              normal: {
-                color: function(params) {
-                  // build a color map as your need.
-                  var colorList = [
-                    "#C1232B",
-                    "#B5C334",
-                    "#FCCE10",
-                    "#E87C25",
-                    "#27727B",
-                    "#FE8463",
-                    "#9BCA63",
-                    "#FAD860",
-                  ];
-
-                  return colorList[params.dataIndex];
-                },
-                opacity: 0.7,
-              },
-            },
-          },
-          //柱顶圆片
-          {
-            name: "",
-            type: "pictorialBar",
-            symbolSize: [60, 22], //调整截面形状
-            symbolOffset: [0, -10],
-            z: 12,
-            symbolPosition: "end",
-            itemStyle: {
-              normal: {
-                color: function(params) {
-                  var colorArr = [
-                    "#C1232B",
-                    "#B5C334",
-                    "#FCCE10",
-                    "#E87C25",
-                    "#27727B",
-                    "#FE8463",
-                    "#9BCA63",
-                  ];
-                  return colorArr[params.dataIndex];
-                },
-              },
-            },
-          },
-        ],
+        series: [],
       },
       normal3D: {
         series: [],
@@ -199,7 +134,7 @@ export default {
     };
   },
   computed: {
-    // 是否需要坐标
+    // 3d还是饼图
     options() {
       return this.isAxisChart ? this.echart3D : this.normal3D;
     },
@@ -207,6 +142,7 @@ export default {
   mounted() {
     // 初始化
     this.initChart();
+    // 窗口大小改变，适配大小
     window.addEventListener("resize", this.resizeChart);
     this.resizeChart();
   },
@@ -214,7 +150,7 @@ export default {
     window.removeEventListener("resize", this.resizeChart);
   },
   methods: {
-    //判断没有初始化
+    //判断echart有没有初始化
     initChart() {
       this.initChartData();
       if (this.echart) {
@@ -223,7 +159,6 @@ export default {
       } else {
         //没有初始化 先初始化  在使用
         var echarts = require("echarts");
-        console.log(echarts, "reree");
         this.echart = echarts.init(this.$refs.echart);
         this.echart.setOption(this.options);
       }
@@ -231,21 +166,166 @@ export default {
     resizeChart() {
       this.echart ? this.echart.resize() : "";
     },
+    //
     initChartData() {
       if (this.isAxisChart) {
-        this.echart3D.xAxis[0].data = this.option.xData;
-        console.log(this.option.series, "this.option.series");
-        console.log(this.echart3D.series, "llll");
-        this.echart3D.series.map((item) => {
-          console.log(item);
-          item.data = this.option.series;
+        let data = {};
+        // 判断 传入的 series 长度 循环赋值给到 this.echart3D.series
+        data = this.seriesData();
+        // 对 this.echart3D.series  进行初始化  赋值
+        this.option.series.forEach(() => {
+          this.echart3D.series.push(...JSON.parse(JSON.stringify(data)));
+        });
+
+        this.echart3D.series.map((item, index) => {
+          // index 0 -2 代表一根圆柱  一次类推  后面看下有无办法，，修改为循环赋值
+          // 对 第一根柱子赋值
+          var len = this.echart3D.series.length / 3;
+          console.log(len, "pppp");
+          var Type = false;
+          console.log(Type);
+          var nums = 1;
+          console.log(index, len * 3, "len * 3");
+          if (index <= 2) {
+            this.judge(item, this.option, 0);
+            if (len !== nums) {
+              Type = true;
+              console.log(Type, 1231231);
+            }
+          } else if (index > 2 && index <= 5) {
+            // 对 第2根柱子赋值
+            this.judge(item, this.option, 1);
+          } else if (index > 5 && index <= 8) {
+            // 对 第3根柱子赋值
+            this.judge(item, this.option, 2);
+          }
+          // if (index <= 2) {
+          //   this.judge(item, this.option, 0);
+          //   console.log();
+          // } else if (index > 2 && index <= 5) {
+          //   // 对 第2根柱子赋值
+          //   this.judge(item, this.option, 1);
+          // } else if (index > 5 && index <= 8) {
+          //   // 对 第3根柱子赋值
+          //   this.judge(item, this.option, 2);
+          // }
+          // 如果 有两根圆柱那么修改  所有圆柱的 偏移量
+          let length = this.echart3D.series.length;
+          if (length > 3 && length <= 6) {
+            switch (index) {
+              case 1:
+                item.symbolOffset = ["-50%", "50%"]; //第一个圆柱下面的园
+                break;
+              case 2:
+                item.symbolOffset = ["-50%", "-50%"]; //第一个圆柱上面的园
+                break;
+              case 4:
+                item.symbolOffset = ["50%", "50%"];
+                break;
+              case 5:
+                item.symbolOffset = ["50%", "-50%"];
+                break;
+            }
+          } else if (length > 6 && length <= 9) {
+            switch (index) {
+              case 1:
+                item.symbolOffset = ["-100%", "50%"]; //第一个圆柱下面的园
+                break;
+              case 2:
+                item.symbolOffset = ["-120%", "-50%"]; //第一个圆柱上面的园
+                break;
+              case 4:
+                item.symbolOffset = ["0%", "50%"];
+                break;
+              case 5:
+                item.symbolOffset = ["0%", "-50%"];
+                break;
+              case 7:
+                item.symbolOffset = ["100%", "-50%"];
+                break;
+              case 8:
+                item.symbolOffset = ["120%", "-50%"];
+                break;
+            }
+          }
           return item;
         });
-        // this.echart3D.series =  this.option.series
+        // x轴数据
+        this.echart3D.xAxis[0].data = this.option.xData;
+        //  xy轴 标题 颜色
+        this.echart3D.yAxis[0].axisLabel.textStyle.color = this.option.XYTitleColor;
+        this.echart3D.xAxis[0].axisLabel.textStyle.color = this.option.XYTitleColor;
+        this.echart3D.title.textStyle.color = this.option.XYTitleColor;
+        this.echart3D.legend.textStyle.color = this.option.XYTitleColor;
+        this.echart3D.title.text = this.option.title;
       } else {
+        // 如果进来的是饼图
         this.normal3D = this.option;
-        console.log(this.option, "this.option-----");
       }
+    },
+    judge(item, option, num) {
+      if (this.option.series[num] !== undefined) {
+        item.data = option.series[num].data;
+        item.itemStyle.normal.color = option.series[num].color;
+        item.name = option.series[num].name;
+      }
+    },
+    // 初始数据
+    seriesData() {
+      let seriesData = [];
+      seriesData.push(
+        {
+          z: 1,
+          name: "",
+          type: "bar",
+          barWidth: 15,
+          barGap: "0%",
+          data: [],
+          itemStyle: {
+            normal: {
+              color: "red",
+            },
+          },
+        },
+        {
+          z: 2,
+          name: "",
+          type: "pictorialBar",
+          data: [],
+          symbol: "diamond",
+          symbolOffset: ["-0%", "50%"],
+          symbolSize: [15, 10],
+          itemStyle: {
+            normal: {
+              color: "red",
+            },
+          },
+          tooltip: {
+            show: false,
+          },
+        },
+        {
+          z: 3,
+          name: "",
+          type: "pictorialBar",
+          symbolPosition: "end",
+          data: [],
+          symbol: "diamond",
+          symbolOffset: ["-0%", "-50%"],
+          symbolSize: [15 - 4, (10 * (15 - 4)) / 15],
+          itemStyle: {
+            normal: {
+              borderColor: "#8bead4",
+              borderWidth: 2,
+              color: "red",
+            },
+          },
+          tooltip: {
+            show: false,
+          },
+        }
+      );
+      return seriesData;
     },
   },
 };
@@ -255,5 +335,6 @@ export default {
 .Echart {
   width: 100%;
   height: 100%;
+  // color: blue;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="content">
-   <div> <Echart :option="option"></Echart></div>
-    <div><Echart :option="pie" :isAxisChart = "false"></Echart></div>
+    <div><Echart :option="option"></Echart></div>
+    <div><Echart :option="option1"></Echart></div>
+    <div><Echart :option="pie" :isAxisChart="false"></Echart></div>
+    <div><Echart :option="option2"></Echart></div>
   </div>
 </template>
 
@@ -14,60 +16,107 @@ export default {
   data() {
     return {
       option: {
-        xData: ["可研数据", "测试2", "测试3", "测试4", "测试5", "测试6"],
-        series:["1030", "205", "84", "568", "689", "4587"],
-        // colorList : [ "#C1232B", "#B5C334","#FCCE10","#E87C25","#27727B", "#FE8463","#9BCA63", "#FAD860", ]
+        xData: ["1月", "2月", "3月", "4月", "5月", "6月", "7月"],
+        title: "标题",
+        series: [
+          // {
+          //   color: "#01FFFF",
+          //   name: "新增",
+          //   data: ["1030", "205", "84", "568", "689", "457"],
+          // },
+          {
+            color: "#1C68FF",
+            name: "删除",
+            data: ["100", "102", "114", "268", "68", "257", "400"],
+          },
+        ],
+        XYTitleColor: "#cccccc",
+      },
+      option2: {
+        xData: ["1", "2", "3", "4", "5", "6"],
+        series: [
+          {
+            color: "red",
+            name: "新增测试",
+            data: ["130", "205", "18", "568", "689", "457"],
+          },
+          {
+            color: "#1C68FF",
+            name: "删除",
+            data: ["100", "122", "114", "268", "68", "257"],
+          },
+          {
+            color: "#ccc",
+            name: "编辑",
+            data: ["1002", "1222", "1142", "2682", "682", "2527"],
+          },
+        ],
+        XYTitleColor: "#000",
+      },
+      option1: {
+        xData: ["1", "2", "3", "4", "5", "6"],
+        title: "标题测试",
+        series: [
+          {
+            color: "#ccc",
+            name: "删除",
+            data: ["100", "122", "114", "268", "68", "257"],
+          },
+          {
+            color: "#1C68FF",
+            name: "编辑",
+            data: ["1002", "1222", "1142", "2682", "682", "2527"],
+          },
+        ],
+        XYTitleColor: "#000",
       },
       pie: {
-          title: {
-            text: "某站点用户访问来源",
-            subtext: "纯属虚构",
-            left: "center",
-          },
-          tooltip: {
-            show: true,
-            trigger: "item",
-            formatter: "{a} <br/>{b} : {c} ({d}%)",
-          },
-          legend: {
-            orient: "vertical",
-            left: "left",
-            data: ["直接访问", "邮件营销", "联盟广告", "视频广告", "搜索引擎"],
-          },
-          series: [
-            {
-              name: "访问来源",
-              type: "pie",
-              radius: "55%",
-              center: ["50%", "60%"],
-              data: [
-                { value: 335, name: "直接访问" },
-                { value: 310, name: "邮件营销" },
-                { value: 234, name: "联盟广告" },
-                { value: 135, name: "视频广告" },
-                { value: 1548, name: "搜索引擎" },
-              ],
-              // emphasis: {
-              //   itemStyle: {
-              //     shadowBlur: 10,
-              //     shadowOffsetX: 0,
-              //     shadowColor: "rgba(0, 0, 0, 0.5)"
-              //   }
-              // }
-            },
-          ],
+        title: {
+          text: "测试",
+          subtext: "纯属虚构",
+          left: "center",
         },
+        tooltip: {
+          show: true,
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)",
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: ["直接访问", "邮件营销", "联盟广告", "视频广告", "搜索引擎"],
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              { value: 335, name: "直接访问" },
+              { value: 310, name: "邮件营销" },
+              { value: 234, name: "联盟广告" },
+              { value: 135, name: "视频广告" },
+              { value: 1548, name: "搜索引擎" },
+            ],
+          },
+        ],
+      },
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.content{
-  display: flex;width: 100%;height: 50vh;
+.content {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100vh;
   justify-content: space-around;
-  >div{
-    width:40%;
+  > div {
+    width: 40%;
+    height: 50%;
   }
 }
 </style>
