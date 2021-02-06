1e1d5c0fdbbd0e87f5743b0f7a48ae46b1b96e33
xiaofei <a871672293@163.com>
2021-02-07 00:40:41 +0800

    修改金字塔

--- src/components/d3/pyramid_4.vue
+++ src/components/d3/pyramid_4.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="svgClass" ref="svgBox">
     <svg :id="config.id" width="100%" height="100%" />
-  </div>
 </template>
 
 <script>
@@ -9,9 +7,10 @@ import * as d3 from "d3";
 export default {
   methods: {
     init() {
-      this.svg = d3.select("#" + this.config.id + "");
+      this.svg = d3.select("#" + this.config.id);
       let width = this.config.width ? this.config.width : 400; //svg  宽
       let height = this.config.height ? this.config.height : 400; //  svg  高
+      console.log(width,height,"oipoipoipo------------")
       let TriangleWidth = this.config.TriangleWidth
         ? this.config.TriangleWidth
         : 30; //三角形 多边形 宽度
@@ -23,169 +22,197 @@ export default {
       let CentreX = width / 3; //模型中心点
       let slant = -4; //三角形左右两侧倾斜度
 
-      let size = 8;//多边形两边的偏移度
+      let size = 8; //多边形两边的偏移度
       let polygonWidth = TriangleWidth; //想分离三角形和多边形的宽度(暂时没想到)
 
       // 三角形起始位置(是以这个位置为绘制点绘制下面的所有图形的Y轴)
       let TriangleCentreY =
-        height / 2 - (polygonHeight * (this.dataList.length-2) - gap) / 2;
-      let textY = 4; //文字y偏移  （文字是以线条定位的，，没有计算到文字的大小，想要到线条的中间需要根据字体大小手动偏向y轴位置）
+        height / 2 - (polygonHeight * (this.dataList.length - 2) - gap) / 2;
+      let textY = 3.5; //文字y偏移  （文字是以线条定位的，，没有计算到文字的大小，想要到线条的中间需要根据字体大小手动偏向y轴位置）
       let textX = 20; //文字x轴偏移   (同上)
 
       //多边形 中间用来体现立体感线条的高度比例
       let nums = 2.5;
       // let cententY = 10;
       // console.log(cententY);
+
+      var tooltip = d3
+        .select("body")
+        .append("div")
+        .attr("class", "tooltip") //用于css设置类样式
+        .attr("opacity", 1);
+
       //三角形
+      //如果是最后一个那么画三角形
+      this.rounded(
+        width,
+        TriangleCentreY +
+          slant +
+          (TriangleCentreY - TriangleCentreY - TriangleWidth)
+      );
+      // this.rounded(TriangleCentreY,index-1,polygonGap,slant,gap,polygonHeight,width)
+      //三角形
+      this.drawTriangle(
+        this.svg,
+        CentreX,
+        TriangleCentreY,
+        TriangleWidth + 5,
+        slant,
+        this.color[0],
+        this.TriangleData[0],
+        tooltip
+      );
+      //用来体现立体感的中间线条
+      this.drawHexagonLine(
+        this.svg,
+        CentreX,
+        TriangleCentreY,
+        CentreX,
+        TriangleCentreY - TriangleWidth * 1.4 - 8,
+      );
+      //三角形右侧线条
+      this.drawLine(
+        this.svg,
+        CentreX + TriangleWidth,
+        TriangleCentreY +
+          slant +
+          (TriangleCentreY - TriangleCentreY - TriangleWidth),
+        width * 0.7,
+        TriangleCentreY +
+          slant +
+          (TriangleCentreY - TriangleCentreY - TriangleWidth)
+      );
+      //三角形标签
+      this.drawTable(
+        this.svg,
+        width * 0.7 + textX,
+        TriangleCentreY +
+          slant +
+          (TriangleCentreY - TriangleCentreY - TriangleWidth) +
+          textY +
+          1,
+        this.TriangleData[0].nama + " " + this.TriangleData[0].num
+      );
       this.dataList.forEach((item, index) => {
-        //如果是最后一个那么画三角形
-        if (this.dataList.length - 1 === index) {
-          //三角形
-          this.drawTriangle(
-            this.svg,
-            CentreX,
-            TriangleCentreY,
-            TriangleWidth + 5,
-            slant,
-            this.color[0]
-          );
-          //用来体现立体感的中间线条
-          this.drawHexagonLine(
-            this.svg,
-            CentreX,
-            TriangleCentreY,
-            CentreX,
-            TriangleCentreY - TriangleWidth * 1.4 - 8,
-            this.color[index]
-          );
-          //三角形右侧线条
-          this.drawLine(
-            this.svg,
-            CentreX + TriangleWidth,
-            TriangleCentreY +
-              slant +
-              (TriangleCentreY - TriangleCentreY - TriangleWidth),
-            width * 0.7,
-            TriangleCentreY +
-              slant +
-              (TriangleCentreY - TriangleCentreY - TriangleWidth)
-          );
-          //三角形标签
-          this.drawTable(
-            this.svg,
-            width * 0.7 + textX,
-            TriangleCentreY +
-              slant +
-              (TriangleCentreY - TriangleCentreY - TriangleWidth) +
-              textY +
-              1,
-            item.nama + " " + item.num
-          );
-        }
         //如果不是最后一个绘制多边形
         //多边形
-        if (this.dataList.length - 1 !== index) {
-          //右侧标签
-          this.drawTable(
-            this.svg,
-            width * 0.7 + textX,
-            TriangleCentreY +
-              index * polygonGap +
+        // if (this.dataList.length - 1 !== index) {
+
+        this.rounded(
+          width,
+          TriangleCentreY +
+            index * polygonGap +
+            slant +
+            gap +
+            (TriangleCentreY +
               slant +
-              gap +
-              (TriangleCentreY +
-                slant +
-                polygonHeight +
-                gap -
-                TriangleCentreY +
-                slant +
-                gap) /
-                3 +
-              textY,
-            item.nama + " " + item.num
-          );
-          //多边形右侧线条
-          this.drawLine(
-            this.svg,
-            CentreX + polygonWidth + index * 10 + size + size,
-            TriangleCentreY +
-              index * polygonGap +
+              polygonHeight +
+              gap -
+              TriangleCentreY +
               slant +
-              gap +
-              -1 +
-              (TriangleCentreY +
-                slant +
-                polygonHeight +
-                gap -
-                3 -
-                TriangleCentreY +
-                slant +
-                gap -
-                1) /
-                3,
-            width * 0.7,
-            TriangleCentreY +
-              index * polygonGap +
+              gap) /
+              3 -
+            2
+        );
+        //右侧标签
+        this.drawTable(
+          this.svg,
+          width * 0.7 + textX,
+          TriangleCentreY +
+            index * polygonGap +
+            slant +
+            gap +
+            (TriangleCentreY +
+              slant +
+              polygonHeight +
+              gap -
+              TriangleCentreY +
+              slant +
+              gap) /
+              3 +
+            textY,
+          item.nama + " " + item.num
+        );
+        //多边形右侧线条
+        this.drawLine(
+          this.svg,
+          CentreX + polygonWidth + index * 10 + size + size,
+          TriangleCentreY +
+            index * polygonGap +
+            slant +
+            gap +
+            -1 +
+            (TriangleCentreY +
+              slant +
+              polygonHeight +
+              gap -
+              3 -
+              TriangleCentreY +
               slant +
               gap -
-              1 +
-              (TriangleCentreY +
-                slant +
-                polygonHeight +
-                gap -
-                3 -
-                TriangleCentreY +
-                slant +
-                gap -
-                1) /
-                3
-          );
+              1) /
+              3,
+          width * 0.7,
+          TriangleCentreY +
+            index * polygonGap +
+            slant +
+            gap -
+            1 +
+            (TriangleCentreY +
+              slant +
+              polygonHeight +
+              gap -
+              3 -
+              TriangleCentreY +
+              slant +
+              gap -
+              1) /
+              3
+        );
 
-          //多边形
-          this.drawHexagon(
-            this.svg,
-            CentreX,
-            polygonWidth + index * 10,
-            TriangleCentreY + index * polygonGap,
-            slant,
-            size,
-            polygonHeight,
-            gap,
-            this.color[index],
-            index
-          );
+        //多边形
+        this.drawHexagon(
+          this.svg,
+          CentreX,
+          polygonWidth + index * 10,
+          TriangleCentreY + index * polygonGap,
+          slant,
+          size,
+          polygonHeight,
+          gap,
+          this.color[index],
+          index,
+          item,
+          tooltip
+        );
+        //多边形中间用来体现立体感的线条
+        //中间线条
+        this.drawHexagonLine(
+          this.svg,
+          CentreX,
+          TriangleCentreY + index * polygonGap + gap + polygonHeight,
+          CentreX,
+          TriangleCentreY + index * polygonGap + (slant + gap) * nums,
+        );
+        //右侧线条
+        this.drawHexagonLine(
+          this.svg,
+          CentreX,
+          TriangleCentreY + index * polygonGap + (slant + gap) * nums,
+          CentreX + polygonWidth + size + index * 10,
+          TriangleCentreY + slant + gap - 1 + index * polygonGap,
+          this.color[index]
+        );
+        //左侧线条
+        this.drawHexagonLine(
+          this.svg,
+          CentreX,
+          TriangleCentreY + index * polygonGap + (slant + gap) * nums,
+          CentreX - polygonWidth - size - index * 10,
+          TriangleCentreY + slant + gap - 1 + index * polygonGap,
+        );
+        // }
 
-          //多边形中间用来体现立体感的线条
-          //中间线条
-          this.drawHexagonLine(
-            this.svg,
-            CentreX,
-            TriangleCentreY + index * polygonGap + gap + polygonHeight,
-            CentreX,
-            TriangleCentreY + index * polygonGap + (slant + gap) * nums,
-            this.color[index]
-          );
-          //右侧线条
-          this.drawHexagonLine(
-            this.svg,
-            CentreX,
-            TriangleCentreY + index * polygonGap + (slant + gap) * nums,
-            CentreX + polygonWidth + size + index * 10,
-            TriangleCentreY + slant + gap - 1 + index * polygonGap,
-            this.color[index]
-          );
-          //左侧线条
-          this.drawHexagonLine(
-            this.svg,
-            CentreX,
-            TriangleCentreY + index * polygonGap + (slant + gap) * nums,
-            CentreX - polygonWidth - size - index * 10,
-            TriangleCentreY + slant + gap - 1 + index * polygonGap,
-            this.color[index]
-          );
-        }
-
-        console.log(item, index);
       });
     },
     //标签的封装
@@ -209,7 +236,9 @@ export default {
       polygonHeight,
       gap,
       color,
-      index
+      index,
+      value,
+      tooltip
     ) {
       svg
         .append("path")
@@ -239,10 +268,16 @@ export default {
         )
         .attr("fill", color)
         .style("opacity", 0.3 + index * 0.1)
-        .on("mouseover", function() {
+        .on("mouseover", function(d) {
+          tooltip
+            .html(value.nama)
+            .style("left", d.clientX + 30 + "px")
+            .style("top", d.clientY - 50 + "px")
+            .style("opacity", 1);
           d3.select(this).style("opacity", 1);
         })
         .on("mouseout", function() {
+          tooltip.style("opacity", 0);
           d3.select(this).style("opacity", 0.4);
         })
         .attr({
@@ -250,8 +285,8 @@ export default {
           "shape-rendering": "crispedges",
         });
     },
-    drawHexagonLine(svg, x1, y1, x2, y2, color) {
-      console.log(color);
+    //模型中间的线条
+    drawHexagonLine(svg, x1, y1, x2, y2) {
       svg
         .append("line")
         .attr("x1", x1)
@@ -260,8 +295,6 @@ export default {
         .attr("y2", y2)
         .attr("stroke", "#fff")
         .style("opacity", 0.1);
-      // .style("font-size", "80%");
-      // .attr("stroke-width", "1px");
     },
     //线条的封装
     drawLine(svg, x1, y1, x2, y2) {
@@ -275,12 +308,21 @@ export default {
         .attr("stroke-width", "1px");
     },
     //顶部三角形封装
-    drawTriangle(svg, CentreX, TriangleCentreY, TriangleWidth, slant, color) {
+    drawTriangle(
+      svg,
+      CentreX,
+      TriangleCentreY,
+      TriangleWidth,
+      slant,
+      color,
+      value,
+      tooltip
+    ) {
       svg
         .append("path")
         .attr(
           "d",
-          `M${CentreX}  ${TriangleCentreY + 4 }
+          `M${CentreX}  ${TriangleCentreY + 4}
           L ${CentreX + TriangleWidth} ${TriangleCentreY + slant}
           L ${CentreX} ${TriangleCentreY - TriangleWidth * 1.4}
           L ${CentreX - TriangleWidth} ${TriangleCentreY + slant}
@@ -288,10 +330,16 @@ export default {
         )
         .attr("fill", color)
         .style("opacity", 0.4)
-        .on("mouseover", function() {
+        .on("mouseover", function(d) {
+          tooltip
+            .html(value.nama)
+            .style("left", d.clientX + 30 + "px")
+            .style("top", d.clientY - 50 + "px")
+            .style("opacity", 1);
           d3.select(this).style("opacity", 1);
         })
         .on("mouseout", function() {
+          tooltip.style("opacity", 0);
           d3.select(this).style("opacity", 0.4);
         })
         .attr({
@@ -299,9 +347,44 @@ export default {
           "shape-rendering": "crispedges",
         });
     },
+    //右侧小圆
+    rounded(width, Y) {
+      //右侧小圆
+      this.svg
+        .append("circle")
+        .attr("cx", width * 0.7 + 10)
+        .attr("cy", Y)
+        .attr("fill", "#fff")
+        .attr("r", 3);
+    },
+    //排序，数量多的放在下面
+    againsTsortKey(array, key) {
+      return array.sort(function(a, b) {
+        var x = a[key];
+        var y = b[key];
+        return x > y ? 1 : x < y ? -1 : 0;
+      });
+    },
+    //窗口大小改变
+    resizeTo(){
+      // console.log(this.config.width,"kkopopio")
+      // console.log(this.config.width,'this.cong')
+      // this.svg = null;
+      // this.init();
+      // console.log("大小改变")
+    }
   },
   mounted() {
+    window.addEventListener("resize",this.resizeTo)
     this.init();
+  },
+  created() {
+    //排序
+    this.againsTsortKey(this.dataList, "num");
+    //三角形里面内容
+    this.TriangleData.push(this.dataList[0]);
+    //排序后把三角形里面内容删除
+    this.dataList.splice(0, 1);
   },
   destroyed() {
     this.svg = null;
@@ -325,19 +408,24 @@ export default {
   data() {
     return {
       svg: null,
+      TriangleData: [],
     };
   },
 };
 </script>
 
-<style>
-.svgClass {
-  width: 500px;
-  height: 500px;
-  /* background: #ccc; */
-  background: url("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2853553659,1775735885&fm=26&gp=0.jpg")
-    no-repeat;
-  background-size: 100% 100%;
-  margin-left: 100px;
+<style lang="scss">
+.tooltip {
+  min-width: 30px;
+  height: 20px;
+  position: absolute;
+  top: 0;
+  opacity: 0;
+  border: 1px solid red;
+  border-radius: 10px;
+  text-align: center;
+  line-height: 20px;
+  padding: 0 8px;
+  color: #fff;
 }
 </style>
--- src/views/d3/test1.vue
+++ src/views/d3/test1.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="height:100vh;width:100vh;display:flex;">
+  <div class="D3" ref="D3">
     <D3_4 :dataList="dataList" :color="color" :config="config"></D3_4>
     <!-- <Rectangle style="width:400px;height:400px;"></Rectangle> -->
     <!-- <div class="iconfont icon-zu6199"></div> -->
@@ -10,16 +10,27 @@
 // import Rectangle from "@/components/d3/rectangle.vue";
 import D3_4 from "@/components/d3/pyramid_4.vue";
 export default {
+  mounted() {
+    window.addEventListener("resize",this.sizeTo)
+  },
   components: {
     D3_4,
     // Rectangle
+  },
+  methods: {
+    sizeTo() {
+      this.config.width = this.$refs.D3.offsetWidth;
+      this.config.height = this.$refs.D3.offsetHeight;
+    console.log(this.config.height, this.config.width, "this.$refs.D3");
+
+    },
   },
   data() {
     return {
       config: {
         id: "svg8",
-        width: 500,
-        height: 500,
+        width: 400,
+        height: 400,
         TriangleWidth: 30, //暂时代表所有的宽度
         polygonGap: 35, //每个多边形的间隙
         polygonHeight: 35, //多边形高度
@@ -35,14 +46,14 @@ export default {
         "red",
       ],
       dataList: [
-        { nama: "火灾", num: "40" },
+        { nama: "火灾21321321", num: "40" },
         { nama: "消防", num: "60" },
         { nama: "刑事案件", num: "10" },
         { nama: "一般事件", num: "30" },
         { nama: "2号", num: "30" },
         { nama: "19号", num: "44" },
-        // { nama: "2号", num: "34" },
-        // { nama: "2号", num: "54" },
+        { nama: "2号", num: "34" },
+        { nama: "2号", num: "54" },
         // { nama: "2号", num: "6454" },
         // { nama: "2号", num: "623" },
         // { nama: "2号", num: "62" },
@@ -54,13 +65,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.icon-zu6199 {
-  width: 500px;
-  height: 500px;
-  position: absolute;
-  opacity: 0.1;
-  font-size: 100px;
-  top: 154px;
-  left: -11px;
+.D3 {
+  height: 45vh;
+  width: 50vh;
+  background: url("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2853553659,1775735885&fm=26&gp=0.jpg");
 }
 </style>
